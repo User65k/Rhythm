c1a6c44cbe9d6451566673cab4eb9c801cd5b533
User65k <15049544+User65k@users.noreply.github.com>
2021-05-06 20:58:12 +0200

    store ctx menu handler

--- rhythm_ui/src/ctx.rs
+++ rhythm_ui/src/ctx.rs
@@ -3,6 +3,7 @@ use wasm_bindgen::JsCast;
 use crate::{log, WebRes, show_error_w_val};
 use web_sys::{Event, Element, MouseEvent, window, HtmlElement, AddEventListenerOptions, Document};
 
+/// event handler that opens the context menu for a row in the request list
 fn context_menu(evt: Event) {
     evt.prevent_default();
     if let Err(e) = move || -> WebRes {
@@ -20,15 +21,15 @@ fn context_menu(evt: Event) {
             menu.style().set_property("display", "")?;
             log(ele.into());
 
-            let ctx_fn = Closure::wrap(Box::new(move |_e: Event| {
+            let ctx_fn = Closure::once_into_js(move |_e: Event| {
                 menu.style().set_property("display", "none");
-            }) as Box<dyn Fn(Event)>);
+            });
+            
             window.add_event_listener_with_callback_and_add_event_listener_options(
                 "click",
-                ctx_fn.as_ref().unchecked_ref(),
+                ctx_fn.unchecked_ref(),
                 AddEventListenerOptions::new().once(true)
             )?;
-            ctx_fn.forget();
         }
         Ok(())
     }() {
@@ -37,17 +38,36 @@ fn context_menu(evt: Event) {
 }
 pub fn setup_ctx_men(document: &Document) -> WebRes
 {
+    //setup callback
     let ctx_fn = Closure::wrap(Box::new(context_menu) as Box<dyn Fn(Event)>);
+    let ucref = unsafe {
+        CTX_MEN_OPEN = Some(ctx_fn.into_js_value());
+        CTX_MEN_OPEN.as_ref().unwrap().unchecked_ref()
+    };
+
+    //add callback to all rows in the request list
     let nodes = document.query_selector_all(".ilist tbody tr")?;
     let mut x = 0;
     while x < nodes.length() {
         let n = nodes.get(x).unwrap();
         n.add_event_listener_with_callback(
             "contextmenu",
-            ctx_fn.as_ref().unchecked_ref()
+            ucref
         )?;
         x+= 1;
     }
-    ctx_fn.forget();
+    Ok(())
+}
+
+static mut CTX_MEN_OPEN: Option<JsValue> = None;
+
+pub fn add_ctx_men(ele: &Element) -> WebRes {
+    let ucref = unsafe {
+        CTX_MEN_OPEN.as_ref().expect("add ctx called before init").unchecked_ref()
+    };
+    ele.add_event_listener_with_callback(
+        "contextmenu",
+        ucref
+    )?;
     Ok(())
 }
